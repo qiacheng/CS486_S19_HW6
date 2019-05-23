bf1afc9abe2f68d71ef89432924612190685954e
qiacheng <choliaky@gmail.com>
2019-05-22 17:27:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import psycopg2 as pgdb
 
 def main():
-    dataB = "s19wdb21"
+    
     hostname= "tanto.cs.pdx.edu"
     username = input("username>")
     password = input("password>")
+    dataB = username
     connection = pgdb.connect(host=hostname, user=username, password=password, sslmode='require')
     db = connection.cursor();
-
-    query = "SELECT * FROM spy.Agent"
+    print("1: Default Query")
+    print("2: Custom Query")
+    print("---------------")
+    answer = input("choice: ")
+    if answer == '1':
+    	query = query = "SELECT * FROM spy.Agent limit 10"
+    elif answer == '2':
+    	query = input("Enter SQL Query: ")
+    else:
+        print("invalid input")
+        return
 
     db.execute(query)
 
